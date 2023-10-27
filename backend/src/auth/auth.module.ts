244066589f926d7b3d32cbaa16148c06cffa8bac
Rafael Vargas <rafael.vargas@gmail.com>
2023-10-27 14:51:50 -0300

    [fix] many bit errors[3~

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { jwtConstants } from './constants';
+import { PrismaService } from 'src/prisma.service';
+import { UsersService } from 'src/users/users.service';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService, PrismaService, UsersService],
+  controllers: [AuthController],
+  exports: [AuthService],
+})
+export class AuthModule {}
