a8c04335000667af0af345252aa7744d2a439592
Rafael Vargas <rafael.vargas@gmail.com>
2023-11-17 00:20:14 -0300

    [update] init sonner lib for testing

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Toaster } from 'sonner';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
         {/* <AuthProvider> */}
         <main>
           {children}
+          <Toaster richColors/>
         </main>
         {/* </AuthProvider> */}
       </body>
